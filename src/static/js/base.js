ec029cf87f42ef2cfa5d83cb92d413cff3f1fc33
Paul Tzouris <tzouris@vermantia.com>
2017-05-25 18:25:26 +0300

    Fixed sticky footer.

--- src/static/js/base.js
+++ src/static/js/base.js
@@ -1,3 +1,5 @@
 $(document).ready(function () {
     $('#ts-interview-content').find('img').addClass('img-fluid');
+    var pb = $('#footer').height() + 70;
+    $('body').css({paddingBottom: pb + 'px'});
 });
